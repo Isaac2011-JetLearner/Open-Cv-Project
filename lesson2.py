cf33e8a1e1c946751334d2db54c5cac6bfe3b1c1
Isaac2011-JetLearner <oliveredosa@yahoo.com>
2026-08-15 12:43:40 +0100

    Add files via upload

--- lesson2.py
+++ lesson2.py
@@ -42,4 +42,27 @@ cv2.imshow("kobe_image",kobe)
 # solid border
 stored_img = cv2.copyMakeBorder(kobe,5,5,5,5,cv2.BORDER_CONSTANT,value= 1)
 cv2.imshow("stored_img",stored_img)
+
+# reflective border
+keep_img = cv2.copyMakeBorder(kobe,50,50,50,50,cv2.BORDER_REFLECT,value= 1)
+cv2.imshow("keep_img",keep_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.imshow("kobe",kobe)
+
+# bluring of a image
+
+# gaussian blur
+blur_img = cv2.GaussianBlur(kobe,(13,13),0)
+cv2.imshow("blur image",blur_img)
+
+
+#median blur
+median_blur = cv2.medianBlur(kobe,13)
+cv2.imshow("median blur",median_blur)
+
+
+#bilateral bluring
+bilateral_blur = cv2.bilateralFilter(kobe,15,100,120)
+cv2.imshow("bilateral blur",bilateral_blur)
 cv2.waitKey(0)
